f0db1ca3d452f424552c526379a94fc5a95d8101
VitorEhrig-Fig <72516899+VitorEhrig-Fig@users.noreply.github.com>
2021-01-20 17:18:19 -0300

    isso aqui oioi

--- example/models.py
+++ example/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
-class Person(models.Model):
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+class Reporter(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField(default=None, blank=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
+
+class Meta:
+        ordering = ['headline']
+
+def __str__(self):
+   return self.headline
