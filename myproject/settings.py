47eef9939af6ad5de25bb26883d95e247b60590b
VitorEhrig-Fig <72516899+VitorEhrig-Fig@users.noreply.github.com>
2021-01-26 17:15:41 -0300

    aea gata bo marka

--- myproject/settings.py
+++ myproject/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
